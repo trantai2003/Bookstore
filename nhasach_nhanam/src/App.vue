ac60244c73cad7c64b7a895cabaf9ac285381696
Trần Đức Tài <102936431+trantai2003@users.noreply.github.com>
2025-07-20 23:26:28 +0700

    feat: create home page

--- nhasach_nhanam/src/App.vue
+++ nhasach_nhanam/src/App.vue
@@ -5,18 +5,15 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+    <!-- <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/book-list">Booklist</RouterLink>
 
       </nav>
-    </div>
+    </div> -->
   </header>
 
   <RouterView />
